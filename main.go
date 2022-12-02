d069cf716d96acc66302821928e018a18e412833
Oliver Galusinski <64497107+OliverGalusinski@users.noreply.github.com>
2022-12-02 13:22:37 +0100

    Getting Docker Containers

--- main.go
+++ main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
 func main() {
-	fmt.Println("Hello World!")
-	log.Output(1, "Hello World logged!")
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		panic(err)
