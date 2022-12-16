62db51f8e1aeb99f41046bd5cebf9c48faac050d
Oliver Galusinski <64497107+OliverGalusinski@users.noreply.github.com>
2022-12-16 09:45:27 +0100

    Update main.go
    Added the Ability to see Logs from other Containers

--- main.go
+++ main.go
@@ -3,23 +3,41 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"strings"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
 func main() {
+	ctx := context.Background()
+
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		panic(err)
 	}
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
 	if err != nil {
 		panic(err)
 	}
 
+	options := types.ContainerLogsOptions{ShowStdout: true}
 	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+		out, err := cli.ContainerLogs(ctx, container.ID, options)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("The \"" + container.Image + "\" container, with the ID \"" + container.ID + "\" logged: ")
+		fmt.Println()
+
+		buf := new(strings.Builder)
+		io.Copy(buf, out)
+		fmt.Println(buf.String())
 	}
+	time.Sleep(time.Minute * 3)
+
 }
