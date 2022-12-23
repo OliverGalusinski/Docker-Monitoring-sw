1e3933e5387394db4e9db043519390394d990cfe
Oliver Galusinski <64497107+OliverGalusinski@users.noreply.github.com>
2022-12-23 13:25:09 +0100

    Update main.go
    + Checking if Database Exists
    + Added two Functions to send to the Database or directly to the Webserver
    + Disected the String into quite a lot of Substrings to check for the Timestamp
    + The programm now checks if the logs are new or old and can understand the Difference
    
    # Have to comment the Code next time

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"io"
+	"log"
 	"strings"
 	"time"
 
@@ -11,33 +13,93 @@ import (
 	"github.com/docker/docker/client"
 )
 
+var containerList = make(map[string]string)
+
 func main() {
+	ctx := context.Background()
+	monitoringDatabaseExists := false
+
+	for {
+		cli, err := client.NewClientWithOpts(client.FromEnv)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//Check if Database Exisits
+		for _, container := range containers {
+			if container.ID == "MonitoringDatabase" {
+				monitoringDatabaseExists = true
+			}
+		}
+
+		if !monitoringDatabaseExists {
+			sendToWebsite()
+		}
+		time.Sleep(time.Second * 3)
+	}
+}
+
+func sendToDatabase() {
+	//Send to Database
+	//Insert Database Code
+}
+
+func sendToWebsite() {
 	ctx := context.Background()
 
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	options := types.ContainerLogsOptions{ShowStdout: true}
+	options := types.ContainerLogsOptions{ShowStdout: true, Timestamps: true}
 	for _, container := range containers {
 		out, err := cli.ContainerLogs(ctx, container.ID, options)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
+		if container.Image != "docker-monitoring-sw" {
+			fmt.Printf("The \"" + container.Image + "\" container, with the ID \"" + container.ID[:10] + "\" logged: ")
+			fmt.Println()
+			buf := new(strings.Builder)
+			io.Copy(buf, out)
 
-		fmt.Printf("The \"" + container.Image + "\" container, with the ID \"" + container.ID + "\" logged: ")
-		fmt.Println()
+			scanner := bufio.NewScanner(strings.NewReader(buf.String()))
+			for scanner.Scan() {
+				inputArray := strings.Split(scanner.Text(), " ")
+				var timeStamp string = inputArray[0]
+				if val, ok := containerList[container.ID]; ok {
+					//Check if already exisiting TimeStamp is before new one
+					timeStampNew, _ := time.Parse(time.RFC3339, timeStamp)
+					timeStampOld, _ := time.Parse(time.RFC3339, val)
 
-		buf := new(strings.Builder)
-		io.Copy(buf, out)
-		fmt.Println(buf.String())
+					if timeStampOld.Unix() < timeStampNew.Unix() {
+						containerList[container.ID] = timeStamp
+						//Send Currend Log
+						for i := 1; i < len(inputArray); i++ {
+							fmt.Print(inputArray[i] + " ")
+						}
+						fmt.Println()
+					}
+				} else {
+					containerList[container.ID] = timeStamp
+				}
+			}
+		}
 	}
-	time.Sleep(time.Minute * 3)
-
+	fmt.Println()
+	for key, value := range containerList {
+		fmt.Println("key: ", key, " Value: ", value)
+	}
+	fmt.Println()
 }
